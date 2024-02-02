a30a4cc36a8e2b8067ef1653c26755cdca17ab85
Shivani110 <negishivani953@gmail.com>
2024-02-02 13:29:08 +0530

    update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,7 +9,8 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
-    public function index(){
-        
+    public function rfgtrg(){
+    
     }
+
 }
